86be5d9b6b80984746ccbec616c066fea201b762
DavMunHer <davidmunyozh@gmail.com>
2024-04-22 17:38:25 +0200

    migracion usuarios funcional

--- server/migrations/20240422140327-createUsersTable.js
+++ server/migrations/20240422140327-createUsersTable.js
@@ -3,7 +3,40 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+        await queryInterface.createTable('users',
+            {
+                id: {
+                    type: Sequelize.INTEGER,
+                    autoIncrement: true,
+                    primaryKey: true
+                },
+                username: {
+                    type: Sequelize.STRING,
+                    allowNull: false
+                },
+                email: {
+                    type: Sequelize.STRING,
+                    unique: true,
+                    validate: {
+                        isEmail: true
+                    }
+                },
+                password: {
+                    type: Sequelize.STRING,
+                    allowNull: false
+                },
+                createdAt: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+                },
+                updatedAt: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+                }
+            }
+        );
     },
 
     async down(queryInterface, Sequelize) {
