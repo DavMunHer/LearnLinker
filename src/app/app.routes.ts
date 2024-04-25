8b57356adbdd322cbf9455dcf9fc7be41a6877ff
DavMunHer <davidmunyozh@gmail.com>
2024-04-25 20:29:41 +0200

    Empezando la navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent,
         title: 'Login'
+    },
+    {
+        path: 'welcome',
+        component: WelcomeComponent,
+        title: 'Welcome'
     },
     // {
     //     path: 'menu',
