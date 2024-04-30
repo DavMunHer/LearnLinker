f34365572c81fb1432158b3ad68689f31fc1740e
DavMunHer <davidmunyozh@gmail.com>
2024-04-30 16:04:20 +0200

    Ya se aceptan peticiones http de parte de Angular hacia Express

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const usersEndpoints = require('./endpoints/users');
+const cors = require('cors');
 
 const express = require('express');
 const app = express(); //Creamos una app de express
 const port = 3000;
 
 app.use(express.json());
+app.use(cors()); //En producción hará falta especificar el origen desde el que se podrá conectar al servidor
 /**
  * Middleware para hacer un parse sobre la información que llega del cliente en formato JSON
  * Se accederá a la información mediante req.body
