368b1b7a988d59197a1984d85081a49d72c6428b
DavMunHer <davidmunyozh@gmail.com>
2024-05-21 19:51:23 +0200

    Ya se crean los usuarios relacionados a una tarea desde el backend

--- server/endpoints/tasks.js
+++ server/endpoints/tasks.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const sequelize = require('../config/database');
 const Project = require('../models/Project');
 const Phase = require('../models/Phase');
 const { Sequelize, Op } = require('sequelize');
 const { formatDateAttribute } = require('./helper');
 const Task = require('../models/Task');
+const User = require('../models/User');
 
 router.post('/create/task', async (req, res) => {
     try {
-        const { name, start_date, deadline, phaseId } = req.body;
-        await Task.create({ name, start_date, deadline, phaseId });
+        const { name, start_date, deadline, phaseId, users } = req.body;
+        const task = await Task.create({ name, start_date, deadline, phaseId });
+        if (users) {
+            for (const user of users) {
+                const developer = await User.findOne({ where: { 'email': user.email } });
+                sequelize.query(`INSERT INTO task_user (userId, taskId) VALUES (${developer.id}, ${task.id});`);
+            }
+        }
         res.status(201).json({ message: 'Task creation successful.' });
     } catch (error) {
         console.error(error);
