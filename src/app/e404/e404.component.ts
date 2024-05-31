78d6bbc675d7c3bd4d078e15321090d1965a43a5
DavMunHer <davidmunyozh@gmail.com>
2024-05-31 17:49:21 +0200

    Estilos de login, signup y el e404

--- src/app/e404/e404.component.ts
+++ src/app/e404/e404.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-e404',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './e404.component.html',
   styleUrl: './e404.component.scss'
 })
