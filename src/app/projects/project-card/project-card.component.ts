47ef36af15adaba9454fd2841eb54d247a4e58fd
DavMunHer <davidmunyozh@gmail.com>
2024-06-01 20:02:42 +0200

    Mejora en el diseño de project-card para mostrar el rol del usuario en el proyecto

--- src/app/projects/project-card/project-card.component.ts
+++ src/app/projects/project-card/project-card.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Project } from '../../../interfaces/project';
-import { DatePipe, TitleCasePipe } from '@angular/common';
+import { DatePipe, NgClass, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
-  imports: [DatePipe, TitleCasePipe],
+  imports: [DatePipe, TitleCasePipe, NgClass],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss'
 })
