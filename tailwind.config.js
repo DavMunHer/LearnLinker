b2d9fe62a0be5f4931a1d8561b1b11037c7cbdde
David Muñoz <dmunyozherrero@gmail.com>
2024-06-03 21:08:09 +0200

    Navbar responsive v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     content: [
-      './src/**/*.{html,ts}',
+      './src/**/*.{html,ts}', "./node_modules/flowbite/*/.js"
     ],
     theme: {
       extend: {},
     },
-    plugins: [],
+    plugins: [require("flowbite/plugin")],
   }
